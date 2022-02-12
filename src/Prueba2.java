421e0aa1e6b6bb31100ac50b4896bd5bd60a4bbb
David <David@DESKTOP-PVUS0LU>
2022-02-12 13:02:28 +0100

    Inicial

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -3,10 +3,8 @@ public class Prueba2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("HolaSDW ");
-           System.out.println("Hola soy Master ");
-           System.out.println("Hola soy Branch1 ");
-           System.out.println("Hola soy Branch1 ");
+           System.out.println("Hola soy UO ");
+           
 	}
 
 }
