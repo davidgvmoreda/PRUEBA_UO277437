5f62c986afd361b3c4cd1edb677980086747590a
David <David@DESKTOP-PVUS0LU>
2022-02-12 13:03:58 +0100

    Modif. RAMA

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -3,7 +3,7 @@ public class Prueba2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("Hola soy UO ");
+           System.out.println("Hola soy UO 277437");
            
 	}
 
