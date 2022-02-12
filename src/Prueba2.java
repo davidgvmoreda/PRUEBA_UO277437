99ee42c0d0c2ebbec2d7e248d0b1a362b7ffd02a
David <David@DESKTOP-PVUS0LU>
2022-02-12 12:36:37 +0100

    Modificacion Rama1

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -3,10 +3,11 @@ public class Prueba2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("HolaSDW ");
+           System.out.println("Hola soy David González Vázquez");
            System.out.println("Hola soy Master ");
            System.out.println("Hola soy Branch1 ");
-           System.out.println("Hola soy Branch1 ");
+          
+           
 	}
 
 }
