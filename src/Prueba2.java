badd01062e4de212ab114975387356cf194e51ad
David <David@DESKTOP-PVUS0LU>
2022-02-12 13:11:57 +0100

    Mod. Master

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -2,9 +2,8 @@
 public class Prueba2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-           System.out.println("Hola soy UO 277437");
-           String a="Hlaa"
+		
+		String author = "David González Vázquez ";
            
 	}
 
