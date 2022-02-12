29aef9f821bb3d4da363a84728d0dff177875636
David <David@DESKTOP-PVUS0LU>
2022-02-12 13:25:22 +0100

    Inicial

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -2,12 +2,8 @@
 public class Prueba2 {
 
 	public static void main(String[] args) {
-		String author = "David González Vázquez vive en Moreda ";
-//<<<<<<< HEAD
-//		String author = "David González Vázquez ";
-//=======
-//		String author = "David González Vázquez vive en Moreda ";
-//>>>>>>> refs/heads/ej2
+		String author = "David González Vázquez ";
+
            
 	}
 
