f0aa6e6cd4eadccbee8173ec196b281216646269
David <David@DESKTOP-PVUS0LU>
2022-02-12 12:44:43 +0100

    Modif Rama

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -3,7 +3,7 @@ public class Prueba2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("HolaSDW ");
+           System.out.println("Hola Branch");
 	}
 
 }
